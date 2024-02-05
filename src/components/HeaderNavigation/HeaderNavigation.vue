7377883c3de72b78361f386fbcfa236ec975a9ea
KingYano <belkadi.yanis.l@gmail.com>
2024-02-05 23:44:46 +0100

    Ajout des images, création des fichiers des composants et réalisation du composant header-navigation

---
+++ src/components/HeaderNavigation/HeaderNavigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="header-navigation">
+    <p class="header-navigation-command header-navigation-previous">Previous</p>
+    <h1 class="header-navigation-title">Historia</h1>
+    <p class="header-navigation-command header-navigation-next">next</p>
+  </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+  @import 'HeaderNavigation';
+</style>
---
+++ src/components/PaintingDetail/PaintingDetail.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="painting-detail">
+
+  </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+  @import 'PaintingDetail';
+</style>
