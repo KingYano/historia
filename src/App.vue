f546dcc01280ccc63ea5a0311c9850a0453f234d
KingYano <belkadi.yanis.l@gmail.com>
2024-02-06 23:50:05 +0100

    Ajout props paintingDetail component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <header-navigation></header-navigation>
+  <painting-detail></painting-detail>
 </template>
 
 <script setup lang="ts">
 import HeaderNavigation from "@/components/HeaderNavigation/HeaderNavigation.vue";
+import PaintingDetail from "@/components/PaintingDetail/PaintingDetail.vue";
 
 </script>
 
--- src/components/PaintingDetail/PaintingDetail.vue
+++ src/components/PaintingDetail/PaintingDetail.vue
@@ -1,10 +1,28 @@
 <template>
   <section class="painting-detail">
-
+    <div class="painting-detail-text">
+      <h2 class="painting-detail-text-title">{{ mainTitle }}</h2>
+      <h3 class="painting-detail-text-subtitle">{{ subTitle }}</h3>
+      <p class="painting-detail-text-location">{{ infoLocation }}</p>
+      <p class="painting-detail-text-characteristic">{{ infoCharacteristic }}</p>
+      <p class="painting-detail-first-paragraph">{{ firstParagraph }}</p>
+      <p class="painting-detail-second-paragraph">{{ secondParagraph }}</p>
+    </div>
+    <div class="painting-detail-image">
+      <img src="" alt="">
+    </div>
   </section>
 </template>
 
 <script setup lang="ts">
+  const props = defineProps({
+    mainTitle: String,
+    subTitle: String,
+    infoLocation: String,
+    infoCharacteristic: String,
+    firstParagraph: String,
+    secondParagraph: String
+  })
 
 </script>
 
