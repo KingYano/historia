abbcf56b54456c272a4788f2f6168ac46b86a2d3
KingYano <belkadi.yanis.l@gmail.com>
2024-02-10 16:17:35 +0100

    Mise en forme du composant PaintingDetail

--- src/components/PaintingDetail/PaintingDetail.vue
+++ src/components/PaintingDetail/PaintingDetail.vue
@@ -5,25 +5,39 @@
       <h3 class="painting-detail-text-subtitle">{{ subTitle }}</h3>
       <p class="painting-detail-text-location">{{ infoLocation }}</p>
       <p class="painting-detail-text-characteristic">{{ infoCharacteristic }}</p>
-      <p class="painting-detail-first-paragraph">{{ firstParagraph }}</p>
-      <p class="painting-detail-second-paragraph">{{ secondParagraph }}</p>
+      <div class="painting-detail-text-paragraph">
+        <p v-for="(firstParagraph, index) in firstParagraphSentences" :key="`first-${index}`" class="first-paragraph">
+          {{ firstParagraph }}
+        </p>
+        <p v-for="(secondParagraph, index) in secondParagraphSentences" :key="`second-${index}`" class="second-paragraph">
+          {{ secondParagraph }}
+        </p>
+      </div>
     </div>
     <div class="painting-detail-image">
-      <img src="" alt="">
+      <img :src="imagePainting" :alt="imageAlt">
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
+  import {computed} from "vue";
+  import {types} from "sass";
+  import String = types.String;
+
   const props = defineProps({
     mainTitle: String,
     subTitle: String,
     infoLocation: String,
     infoCharacteristic: String,
     firstParagraph: String,
-    secondParagraph: String
+    secondParagraph: String,
+    imagePainting: String,
+    imageAlt: String
   })
 
+  const firstParagraphSentences = computed(() => props.firstParagraph?.match(/[^\.!\?]+[\.!\?]+/g) || []);
+  const secondParagraphSentences = computed(() => props.secondParagraph?.match(/[^\.!\?]+[\.!\?]+/g) || []);
 </script>
 
 <style lang="scss">
