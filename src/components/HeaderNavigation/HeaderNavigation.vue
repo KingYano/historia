88f233390649bd9ab14e4e381666d0016914b030
KingYano <belkadi.yanis.l@gmail.com>
2024-02-23 11:06:11 +0100

    Accessibilité

--- src/components/HeaderNavigation/HeaderNavigation.vue
+++ src/components/HeaderNavigation/HeaderNavigation.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="header-navigation">
-    <p tabindex="0" role="button"  class="header-navigation-command header-navigation-previous" @click="goBack">Previous</p>
-    <h1 tabindex="0" role="button" class="header-navigation-title" @click="goHome">Historia</h1>
-    <p tabindex="0" role="button" class="header-navigation-command header-navigation-next" @click="goNext">Next</p>
+    <p tabindex="0" @keydown.enter.prevent="goBack"  class="header-navigation-command header-navigation-previous" @click="goBack">Previous</p>
+    <h1 tabindex="0" @keydown.enter.prevent="goHome" class="header-navigation-title" @click="goHome">Historia</h1>
+    <p tabindex="0" @keydown.enter.prevent="goNext"  class="header-navigation-command header-navigation-next" @click="goNext">Next</p>
   </header>
 </template>
 
